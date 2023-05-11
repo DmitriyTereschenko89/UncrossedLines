9c6eac0761fea2ae093ec763c23596733504dd63
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-05-11 17:26:23 +0300

    update solution

--- Program.cs
+++ Program.cs
@@ -7,7 +7,26 @@
             #region DP Solution with optimized
             public int MaxUncrossedLinesDPOptimized(int[] nums1, int[] nums2)
             {
-                return 0;
+                int n = nums1.Length;
+                int m = nums2.Length;
+                int[] prev = new int[m + 1];
+                for (int i = 0; i < n; ++i)
+                {
+                    int[] dp = new int[m + 1];
+                    for (int j = 0; j < m; ++j)
+                    {
+                        if (nums1[i] == nums2[j])
+                        {
+                            dp[j + 1] = 1 + prev[j];
+                        }
+                        else
+                        {
+                            dp[j + 1] = Math.Max(dp[j], prev[j + 1]);
+                        }
+                    }
+                    prev = dp;
+                }
+                return prev[m];
             }
             #endregion
 
